42291404d1379196a5eed881d98101e6a041efaf
DungNg <87799163+nminhdung@users.noreply.github.com>
2023-06-06 13:08:07 +0700

    login logout experidToken

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,6 +4,14 @@ const {
   generateRefreshToken,
 } = require("../middlewares/jwt");
 const asyncHandler = require("express-async-handler");
+const jwt = require("jsonwebtoken");
+const {
+  findOneAndDelete,
+  findByIdAndUpdate,
+  findOne,
+  findOneAndUpdate,
+} = require("../models/user");
+const sendMail = require("../ultils/sendMail");
 
 const register = asyncHandler(async (req, res) => {
   const { email, password, firstname, lastname } = req.body;
@@ -68,8 +76,30 @@ const login = asyncHandler(async (req, res) => {
     throw new Error("Invalid credentials!");
   }
 });
+const expiredToken = asyncHandler(async (req, res) => {
+  //lay cookie
+  const cookie = req.cookies;
+  //Check co token ko
+  if (!cookie && !cookie.refreshToken) {
+    throw new Error("No refresh token in cookies");
+  } else {
+    //Check token co con` han su dung ko
+    const rs = await jwt.verify(cookie.refreshToken, process.env.JWT_SECRET);
+    //Check token co giong trong db ko
+    const response = await User.findOne({
+      _id: rs._id,
+      refreshToken: cookie.refreshToken,
+    });
+    return res.status(200).json({
+      success: response ? true : false,
+      newAccessToken: response
+        ? generateAccessToken(response._id, response.role)
+        : "Refresh token invalid",
+    });
+  }
+});
 const getUser = asyncHandler(async (req, res) => {
-  console.log(req.user)
+  console.log(req.user);
   const { _id } = req.user;
   // khong hien thi 3 truong trong select
   const user = await User.findById(_id).select("-refreshToken -password -role");
@@ -78,8 +108,64 @@ const getUser = asyncHandler(async (req, res) => {
     result: user ? user : "User not found",
   });
 });
+const logout = asyncHandler(async (req, res) => {
+  const cookie = req.cookies;
+  if (!cookie || !cookie.refreshToken) {
+    throw new Error("No refresh token");
+  } else {
+    //xoa token o db
+    await User.findOneAndUpdate(
+      { refreshToken: cookie.refreshToken },
+      { refreshToken: "" },
+      { new: true }
+    );
+    //xoa token o cookie
+    res.clearCookie("refreshToken", { httpOnly: true, secure: true });
+    return res.status(200).json({ success: true, mes: "Logout is done" });
+  }
+});
+
+// Client gui Email
+// Server check email co hop le khong => server gui email + link (password change token)
+// Client check mail => click link
+// Client gui API kem token
+// Server Check token co giong voi token ma server gui mail hay ko
+// Change password
+
+const forgotPassword = asyncHandler(async (req, res) => {
+  const { email } = req.query;
+  if (!email) {
+    throw new Error("Missing Email");
+  } else {
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw new Error("User not found");
+    }
+    //khi sử dụng hàm đc định nghĩa trong schema phải save() để lưu lại những thay đổi
+    //trong trường hợp hàm createPasswordChangedToken sẽ update 2 field là passwordResetToken và passwordResetExpire
+    const resetToken = user.createPasswordChangedToken();
+    await user.save();
+
+    const html = `Xin vui lòng click vào link dưới đây để thay đổi mật khẩu.Link này chỉ tồn tại trong 15 phút.
+    <a href="${process.env.URL_SERVER}/api/user/reset-password/${resetToken}">CLick here</a> `;
+
+    const data = {
+      email,
+      html,
+    };
+    const rs = await sendMail(data);
+    return res.status(200).json({
+      success: true,
+      rs,
+    });
+  }
+});
+
 module.exports = {
   register,
   login,
+  expiredToken,
   getUser,
+  logout,
+  forgotPassword,
 };
--- server/routes/user.js
+++ server/routes/user.js
@@ -4,6 +4,9 @@ const { verifyAccessToken } = require("../middlewares/verifyToken");
 
 router.post("/register", UserControllers.register);
 router.post("/login", UserControllers.login);
-router.get("/current",verifyAccessToken, UserControllers.getUser);
+router.get("/current", verifyAccessToken, UserControllers.getUser);
+router.post("/refreshtoken", UserControllers.expiredToken);
+router.get("/logout",  UserControllers.logout);
+router.get("/forgotpassword",  UserControllers.forgotPassword);
 
 module.exports = router;
